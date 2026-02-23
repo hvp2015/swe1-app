5cc6af1435b8af3b77c0092989bcbfb6b7f785ae
NoobackerCorporation <noobacker7@gmail.com>
2026-02-23 16:37:00 -0500

    simplify db permissions and enable debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a6^dsb-b-t)=u)0vz_s$&(hpdrm&e_o_g-sjz55dv@fghj*l#z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Allowed hosts for Elastic Beanstalk + local dev
