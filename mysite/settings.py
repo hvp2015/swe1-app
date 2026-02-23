70eb6a1b3d6c51245bde17337ce36cc2219cb700
NoobackerCorporation <noobacker7@gmail.com>
2026-02-23 11:46:43 -0500

    Add EB config and requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,15 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-]
+ALLOWED_HOSTS = ['*']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
